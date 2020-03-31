09d75cb7a9b1092d8c215b11d7961d662ff5afe9
Cesar <mail.mansilla@gmail.com>
2020-03-31 19:12:25 -0300

    Explicación de fórmula

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,27 +1,27 @@
 <template class="mb-5">
   <b-row>
-    <b-col>
-      <h2 class="my-3">Predicción Argentina a 5 días</h2>
-      <b-row>
+    <b-col>      
+      <b-row class="my-3">
         <b-col>
+          <h2>Predicción Argentina a 5 días</h2>
           <b-alert show variant="warning">
             <p>Este modelo no contempla ni los casos recuperados ni las muertes. Apunta a predecir el crecimiento inicial de la pandemia.</p>  
-          </b-alert>          
+          </b-alert>
         </b-col>
       </b-row>
-      <b-row class="mt-3">
+      <b-row class="my-3">
         <b-col>
           <line-chart
             :chart-data="chartdata"
             :options="options"/>
         </b-col>
       </b-row>      
-      <b-row>
+      <b-row class="my-3">
         <b-col>
-          <h2 class="my-3">Resúmen de casos confirmados</h2>
+          <h2>Resúmen de casos confirmados</h2>
         </b-col>
       </b-row>
-      <b-row>
+      <b-row class="my-3">
         <b-col>
           <b-card title="Casos confirmados" class="text-center">
             <b-card-text>
@@ -38,11 +38,36 @@
         </b-col>
         <b-col></b-col>
       </b-row>
-      <b-row class="mt-3">
+      <b-row class="my-3">
         <b-col>
           <b-alert show variant="info">
             <p>Info extraída de <a href="https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases" target="_blank">European Centre for Disease Prevention and Control</a></p>
             <p>Última actualización: {{today()}}</p>
+          </b-alert>
+        </b-col>
+      </b-row>
+      <b-row class="my-3">
+        <b-col>
+          <h2 class="my-3">Explicación de predicción</h2>
+
+          <p>Partimos de <strong>obtener el incremento de casos</strong> para un día dado <i>n</i>, que se obtiene restando el número de casos del día, al número de casos del día siguiente (<i>n + 1</i>)</p>
+          <b-img :src="require(`../assets/formulas/1_calculo_nuevos_infectados.png`)" fluid alt="Cálculo nuevos infectados"></b-img>
+
+          <p>Luego, planteamos una fórmula para el cálculo de infectados en el día <i>n</i>, que responde al grado de exposición <b><i>e</i></b> (gente con la que te cruzás), también responde a la probabilidad de contagio <b><i>p</i></b> (acatamiento a las medidas como lavarse las manos, toser en el pliegue del codo, etc) y también depende de la cantidad de infectados en el día dado <b><i>I<sub>n</sub></i></b>.</p>
+          <p>Con todo esto se plantea la fórmula: </p>
+          <b-img :src="require(`../assets/formulas/2_calculo_causas_nuevos_infectados.png`)" fluid alt="Cálculo nuevos infectados"></b-img>
+
+          <p>Igualamos ambas fórmulas anteriores para detectar el <strong>factor de incremento diario</strong>.</p>
+          <b-img :src="require(`../assets/formulas/3_deteccion_factor_incremento.png`)" fluid alt="Detección de factor de incremento diario"></b-img>
+
+          <p>Lo que se concluye en este punto es que la cantidad de infectados de mañana, será igual a la cantidad de infectados de hoy, multiplicada por el factor de incremento diario <b><i>e * p + 1</i></b>. Llamaremos a este factor <b><i>F</i></b></p>
+          <b-img :src="require(`../assets/formulas/5_factor_incremento.png`)" fluid alt="Fórmula factor de incremento diario"></b-img>
+
+          <p>Reemplazamos este factor <b><i>F</i></b> en la fórmula obtenida en el primer despeje, despejamos el factor, y esa fórmula es la aplicada en el modelo de este ejercicio, es decir, los infectados del día de hoy, dividido los infectados de ayer. Iterando y multiplicando por este factor es que se realiza la predicción.</p>
+          <b-img :src="require(`../assets/formulas/6_formula_factor_incremento.png`)" fluid alt="Fórmula factor de incremento diario para modelo"></b-img>
+
+          <b-alert show variant="warning">
+            <p>Lo que resumí acá, pueden verlo perfectamente explicado en el video de <strong>El Traductor de Ingeniería</strong>: <a href="https://www.youtube.com/watch?v=-PUT0hZiZEw" target="_blank">Te Explico POR QUÉ estoy PREOCUPADO | [COVID-19]</a></p>  
           </b-alert>
         </b-col>
       </b-row>
